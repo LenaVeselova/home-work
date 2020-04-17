b181a17eb4ec317d82de34124f971c35192c76e9
LenaVeselova <lena.veselova88@gmail.com>
2020-04-17 18:51:19 +0300

    Updates

---
+++ precache-manifest.f97a7a865a3715880a9ef5be21590817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5509cfeac155d788e164f8365980f4",
+    "url": "/home-work/index.html"
+  },
+  {
+    "revision": "5adae259f668b4d07623",
+    "url": "/home-work/static/css/main.8597572d.chunk.css"
+  },
+  {
+    "revision": "dbe0e69b6d127b0aff8c",
+    "url": "/home-work/static/js/2.426e8f44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home-work/static/js/2.426e8f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5adae259f668b4d07623",
+    "url": "/home-work/static/js/main.0e4d5386.chunk.js"
+  },
+  {
+    "revision": "2e829a2fa29d1094a68b",
+    "url": "/home-work/static/js/runtime-main.f42fca81.js"
+  }
+]);
